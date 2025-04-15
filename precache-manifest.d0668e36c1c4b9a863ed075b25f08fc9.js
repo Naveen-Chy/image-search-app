6592d6028523e3ded3cf778a57b657f0dd51f80c
Naveen-Chy <naveenitboy@gmail.com>
2025-04-15 16:23:21 +0530

    Updates

---
+++ precache-manifest.d0668e36c1c4b9a863ed075b25f08fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1091f661b624f3df1cc77c7d66a8dc3",
+    "url": "/image-search-app/index.html"
+  },
+  {
+    "revision": "e57bc53d137f996c3252",
+    "url": "/image-search-app/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "e5f3b47da01ddc6a9dad",
+    "url": "/image-search-app/static/css/main.d5fd057b.chunk.css"
+  },
+  {
+    "revision": "e57bc53d137f996c3252",
+    "url": "/image-search-app/static/js/2.c219f83d.chunk.js"
+  },
+  {
+    "revision": "8aca329d31ca141c0240",
+    "url": "/image-search-app/static/js/3.44ff5753.chunk.js"
+  },
+  {
+    "revision": "e5f3b47da01ddc6a9dad",
+    "url": "/image-search-app/static/js/main.9c1ec942.chunk.js"
+  },
+  {
+    "revision": "2dce697333098a91c2ae",
+    "url": "/image-search-app/static/js/runtime~main.ade65b76.js"
+  }
+]);
